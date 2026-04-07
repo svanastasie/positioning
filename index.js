882c2d82272a97ce33881f11e91e8a0b00533c58
Anastasia Svalova <svanastasie@yandex.ru>
2026-04-07 19:41:18 +0500

    add modal window

--- index.js
+++ index.js
@@ -5,3 +5,15 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const modal = document.querySelector('.modal');
+const openButton = document.querySelector('.open-button');
+const closeButton = document.querySelector('.close-icon');
+
+openButton.onclick = () => {
+    modal.style.display = 'flex';
+}
+
+closeButton.onclick = () => {
+    modal.style.display = 'none';
+}
